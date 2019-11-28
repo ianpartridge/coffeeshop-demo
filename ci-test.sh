a9c73ce65b90311ec7d7f2e2b5086fb0eb9e54f1
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-11-28 09:52:55 +0000

    Bump up the timeout again

--- ci-test.sh
+++ ci-test.sh
@@ -14,13 +14,12 @@ else
 fi
 
 # Test order completion
-eventStream=`curl -s --max-time 8 http://localhost:8080/queue | grep 'data'`
+eventStream=`curl -s --max-time 10 http://localhost:8080/queue | grep 'data'`
 responseRegex='"state" *: *"READY"'
 
 if [[ "$eventStream" =~ $responseRegex ]]; then
   echo "Order was successfully processed"
 else
   echo "Order completion not detected"
-  docker-comopse logs barista-kafka
   false
 fi
