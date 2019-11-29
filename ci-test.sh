1bf1baa19eca95f6c88afba3309044f59fd8c695
David Jones <djones6@uk.ibm.com>
2019-11-29 11:05:26 +0000

    Make service port configurable
    Add liveness and readiness checks
    
    Wait for pod readiness before testing

--- ci-test.sh
+++ ci-test.sh
@@ -2,8 +2,11 @@
 
 set -ex
 
+# First argument customizes service port (default, 8080)
+SERVICE_PORT=${1:-8080}
+
 # Test POSTing an order
-response=`curl -s -d"{\"name\":\"Travis\",\"product\":\"frappuccino\"}" -H "Content-Type: application/json" http://localhost:8080/messaging`
+response=`curl -s -d"{\"name\":\"Travis\",\"product\":\"frappuccino\"}" -H "Content-Type: application/json" http://localhost:${SERVICE_PORT}/messaging`
 responseRegex='orderId" *: *"[a-z0-9-]+"'
 
 if [[ "$response" =~ $responseRegex ]]; then
@@ -13,8 +16,10 @@ else
   false
 fi
 
-# Test order completion
-eventStream=`curl -s --max-time 6 http://localhost:8080/queue | grep 'data'`
+# Test order completion. Wait for 6 seconds for a response in the stream
+# (it should only take around 5 seconds for barista-kafka to respond, but
+# may take longer in a CI environment)
+eventStream=`curl -s --max-time 6 http://localhost:${SERVICE_PORT}/queue | grep 'data'`
 responseRegex='"state" *: *"READY"'
 
 if [[ "$eventStream" =~ $responseRegex ]]; then
