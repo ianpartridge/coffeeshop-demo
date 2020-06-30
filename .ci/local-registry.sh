9dfdafc5458dd254db7b45c049471875e5355e4d
Matthew Wilson <matthew.wilson@uk.ibm.com>
2020-06-30 13:46:59 +0100

    Add namespace to images

--- .ci/local-registry.sh
+++ .ci/local-registry.sh
@@ -3,7 +3,7 @@
 # Push locally built images to the local registry that was created
 # by local-cluster.sh
 
-images="coffeeshop-demo:barista-kafka coffeeshop-demo:barista-http coffeeshop-demo:coffeeshop-service"
+images="ianpartridge/coffeeshop-demo:barista-kafka ianpartridge/coffeeshop-demo:barista-http ianpartridge/coffeeshop-demo:coffeeshop-service"
 registry="localhost:5000"
 
 for image in $images; do
