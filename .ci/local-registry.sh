4759d70abb76b73ea8e343eef9ff407c0e3bbe82
Matthew Wilson <matthew.wilson@uk.ibm.com>
2020-06-30 11:16:29 +0100

    Update image tags for test deployment

--- .ci/local-registry.sh
+++ .ci/local-registry.sh
@@ -3,7 +3,7 @@
 # Push locally built images to the local registry that was created
 # by local-cluster.sh
 
-images="barista-kafka barista-http coffeeshop-service"
+images="coffeshop-demo:barista-kafka coffeeshop-demo:barista-http coffeeshop-demo:coffeeshop-service"
 registry="localhost:5000"
 
 for image in $images; do
