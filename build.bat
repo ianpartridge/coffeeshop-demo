5eb2f53fb91055411ce07bf7ef9f03c666ece311
Matthew Wilson <matthew.wilson@uk.ibm.com>
2020-06-30 14:04:33 +0100

    Avoid explicit docker.io so CI works

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-docker build -f barista-kafka\Dockerfile -t barista-kafka .
-docker build -f barista-http\Dockerfile -t barista-http .
-docker build -f coffeeshop-service\Dockerfile -t coffeeshop-service .
+docker build -f barista-kafka\Dockerfile -t ianpartridge/coffeeshop-demo:barista-kafka .
+docker build -f barista-http\Dockerfile -t ianpartridge/coffeeshop-demo:barista-http .
+docker build -f coffeeshop-service\Dockerfile -t ianpartridge/coffeeshop-demo:coffeeshop-service .
--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Build 3 images (in parallel)
 
-docker build -f barista-kafka/Dockerfile -t coffeeshop-demo:barista-kafka . > build1.log 2>&1 &
+docker build -f barista-kafka/Dockerfile -t ianpartridge/coffeeshop-demo:barista-kafka . > build1.log 2>&1 &
 BUILD1=$!
-docker build -f barista-http/Dockerfile -t coffeeshop-demo:barista-http . > build2.log 2>&1 &
+docker build -f barista-http/Dockerfile -t ianpartridge/coffeeshop-demo:barista-http . > build2.log 2>&1 &
 BUILD2=$!
-docker build -f coffeeshop-service/Dockerfile -t coffeeshop-demo:coffeeshop-service .
+docker build -f coffeeshop-service/Dockerfile -t ianpartridge/coffeeshop-demo:coffeeshop-service .
 
 wait $BUILD1 && cat build1.log && rm build1.log
 wait $BUILD2 && cat build2.log && rm build2.log
